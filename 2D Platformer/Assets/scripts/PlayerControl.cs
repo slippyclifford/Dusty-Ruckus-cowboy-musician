826095ff9ec1182ad0b7f8e4e31fbb183e44d21d
Hayes Geldmacher <hayes.g850@gmail.com>
2018-09-12 10:27:05 -0600

    Added a jump and move mechanic. My life is now validated.

--- 2D Platformer/Assets/scripts/PlayerControl.cs
+++ 2D Platformer/Assets/scripts/PlayerControl.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour {
+public class PlayerControl : MonoBehaviour
+{
+    // Player Movement Variables
+    public int Movespeed;
+    public float Jumpheight;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //Player grounded variables 
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask WhatIsGround;
+    private bool grounded;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, WhatIsGround);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //This code makes the character jump
+        if (Input.GetKeyDown(KeyCode.Space) & grounded)
+        {
+            Jump();
+        }
+        //this code allows the player to move from side to side using the "a" and "d" keys. 
+        if(Input.GetKey (KeyCode.D)){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(Movespeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if(Input.GetKey (KeyCode.A)){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-Movespeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+
+    public void Jump()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, Jumpheight);
+    }
 }
