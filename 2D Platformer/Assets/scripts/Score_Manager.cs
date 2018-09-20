e0c40bda019622edc4cbb99bac49e5d7db387de1
Hayes Geldmacher <hayes.g850@gmail.com>
2018-09-20 10:28:26 -0600

    Added Death box and score manager script

---
+++ 2D Platformer/Assets/scripts/Score_Manager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score_Manager : MonoBehaviour
+{
+
+    public static int Score;
+
+    Text ScoreText;
+
+    //Use this for initialization
+    void Start()
+    {
+        ScoreText = GetComponent<Text>();
+
+        Score = 0;
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+        if (Score < 0)
+            Score = 0;
+        ScoreText.text = " " + Score; 
+	}
+
+    public static void AddPoints (int PointsToAdd){
+        Score += PointsToAdd; 
+    }
+
+    public static void Reset (){
+        
+    }
+
+
+
+
+
+
+}
---
+++ 2D Platformer/Assets/scripts/deathbox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathbox : MonoBehaviour{
+
+    void OnTriggerEnter2D(Collider2D other){
+
+        if (other.name == "Pc")
+        {
+            Debug.Log("Pc Enters Death Zone");
+            Destroy(other);
+        }
+
+    }
+}
