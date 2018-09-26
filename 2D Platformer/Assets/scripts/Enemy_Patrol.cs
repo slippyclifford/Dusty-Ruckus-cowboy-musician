b6ffc2a9d659fc024b42be08bdde74e5f965596d
Hayes Geldmacher <hayes.g850@gmail.com>
2018-09-26 10:29:17 -0600

    Added Enemy

---
+++ 2D Platformer/Assets/scripts/Enemy_Patrol.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Patrol : MonoBehaviour
+{
+
+    //Movement Variables
+    public float MoveSpeed;
+    public bool MoveRight;
+
+    //Wall Check
+    public Transform WallCheck;
+    public float WallCheckRadius;
+    public LayerMask WhatIsWall;
+    private bool HittingWall;
+    private bool NotAtEdge;
+    public Transform EdgeCheck;
+
+    //Update is called once per frame
+    void Update()
+    {
+        NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
+        HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+        if (HittingWall || !NotAtEdge)
+        {
+            MoveRight = !MoveRight;
+        }
+        if (MoveRight)
+        {
+            transform.localScale = new Vector3(-3f, 3f, 3f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else
+        {
+            transform.localScale = new Vector3(3f, 3f, 3f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+    }
+}
