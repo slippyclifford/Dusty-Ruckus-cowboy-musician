fc76022237a4168ccb887d480c4e799d200593d5
Hayes Geldmacher <hayes.g850@gmail.com>
2018-10-22 10:09:40 -0600

    kill floor

--- 2D Platformer/Assets/scripts/PlayerShoot.cs
+++ 2D Platformer/Assets/scripts/PlayerShoot.cs
@@ -13,7 +13,7 @@ public class PlayerShoot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.RightControl))
+        if(Input.GetKeyDown(KeyCode.RightControl))
             Instantiate(Projectile, FirePoint.position, FirePoint.rotation);
 	}
 }
