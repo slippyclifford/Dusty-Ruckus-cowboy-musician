95ab18840f7bd4da401e293318776bd41bbd92c4
Hayes Geldmacher <hayes.g850@gmail.com>
2018-10-30 10:25:30 -0600

    Added a smoother camera

---
+++ 2D Platformer/Assets/scripts/Camera_follow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_follow : MonoBehaviour {
+   
+    public PlayerControl Pc;
+
+    public bool isfollowing;
+
+    //camera position offset
+    public float xoffset;
+
+    public float yoffset; 
+
+    //use this for initialization
+    void Start () {
+        Pc = FindObjectOfType<PlayerControl>();
+        isfollowing = true;
+    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(isfollowing){
+            transform.position = new Vector3(Pc.transform.position.x + xoffset, Pc.transform.position.y + yoffset, transform.position.z);
+        }
+	}
+}
