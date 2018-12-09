ad15634be620748706f210f187e998563a284810
Hayes Geldmacher <hayes.g850@gmail.com>
2018-12-08 23:43:42 -0700

    fixed enemy patrol

--- 2D Platformer/Assets/scripts/EnemyPatrol.cs
+++ 2D Platformer/Assets/scripts/EnemyPatrol.cs
@@ -34,12 +34,12 @@ public class EnemyPatrol : MonoBehaviour
 
         if (MoveRight)
         {
-            transform.localScale = new Vector3(-3f, 3f, 1f);
+            transform.localScale = new Vector3(3f, 3f, 1f);
             GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
         else
         {
-            transform.localScale = new Vector3(3f, 3f, 1f);
+            transform.localScale = new Vector3(-3f, 3f, 1f);
             GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
     }
